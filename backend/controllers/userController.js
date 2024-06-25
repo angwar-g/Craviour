309473635bb2eabad70ec89607072d6058c24a6f
Amishi Gangwar <92440009+angwar-g@users.noreply.github.com>
2024-06-25 10:03:55 +0530

    user authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import { response } from "express";
+
+// LOGIN USER
+const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await userModel.findOne({email})
+
+        if (!user) {
+            return res.json({success:false, message: "User does not exist"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({success:false, message: "Invalid credentials"});
+        }
+
+        // CREATE TOKEN IF PASSWORD MATCHES
+        const token = createToken(user._id);
+        res.json({success:true, token});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Something went wrong"});
+    }
+    
+
+}
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET);
+}
+
+//REGISTER USER
+const registerUser = async (req, res) => {
+    const {name , password, email} = req.body;
+    try {
+        // CHECK IF USER EXISTS
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false, message: "User already exists"});
+        }
+
+        // VALIDATE EMAIL AND PASSWORD STRENGTH
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message: "Enter a valid email"});
+        }
+
+        if (password.length < 8) {
+            return res.json({success:false, message: "Password is too short"});
+        }
+
+        // HASHING PASSWORD
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashedPassword
+        });
+
+        const user = await newUser.save();
+        const token = createToken(user._id);
+        res.json({success:true, token});
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Something went wrong"});
+    }
+
+}
+
+export { loginUser, registerUser };
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
-
+import userRouter from './routes/userRoute.js'
+import 'dotenv/config'
 
 // APP CONFIG
 const app = express()
@@ -18,6 +19,7 @@ connectDB()
 // api endpoints
 app.use('/api/food', foodRouter)
 app.use('/images', express.static('uploads'))
+app.use('/api/user', userRouter)
 
 // when we hit the '/' endpoint, we will get a response of 'API Working'
 app.get('/', (req, res) => {
